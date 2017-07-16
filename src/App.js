62a5c9bc769ad01ae63945ac13198aa32e197a8f
webbdm <gwebb762@gmail.com>
2017-07-15 21:53:21 -0500

    Displaying Islands on main page from Firebase

--- src/App.js
+++ src/App.js
@@ -16,13 +16,31 @@ class App extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(e){
+  componentDidMount() {
+    const islandsRef = firebase.database().ref('islands');
+    islandsRef.on('value', (snapshot) => {
+      let islands = snapshot.val();
+      let newState = [];
+      for (let island in islands) {
+        newState.push({
+          id: island,
+          islandLocation: islands[island].islandLocation,
+          islandname: islands[island].islandname
+        });
+      }
+      this.setState({
+        islands: newState
+      });
+    });
+  }
+
+  handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value
     });
   }
 
-  handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
     const islandsRef = firebase.database().ref('islands');
     const island = {
@@ -48,14 +66,22 @@ class App extends Component {
         <div className='container'>
           <section className='add-item'>
             <form onSubmit={this.handleSubmit}>
-              <input type="text" name="islandname" placeholder="What's the island name?" onChange={this.handleChange}  value={this.state.islandname}/>
-              <input type="text" name="islandLocation" placeholder="Where is it?" onChange={this.handleChange} value={this.state.islandLocation}/>
+              <input type="text" name="islandname" placeholder="What's the island name?" onChange={this.handleChange} value={this.state.islandname} />
+              <input type="text" name="islandLocation" placeholder="Where is it?" onChange={this.handleChange} value={this.state.islandLocation} />
               <button>Add Island</button>
             </form>
           </section>
           <section className='display-item'>
             <div className='wrapper'>
               <ul>
+                {this.state.islands.map((island) => {
+                  return (
+                    <li key={island.id}>
+                      <h3>{island.islandname}</h3>
+                      <p>{island.islandLocation}</p>
+                    </li>
+                  )
+                })}
               </ul>
             </div>
           </section>
