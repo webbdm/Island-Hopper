8703ab929ee59a3492fc67ff25d69b2764ed904a
webbdm <gwebb762@gmail.com>
2017-09-08 20:37:39 -0500

    Added goals

--- src/Goals.js
+++ src/Goals.js
@@ -3,10 +3,26 @@ import './App.css';
 //import { Link } from 'react-router-dom';
 import firebase from './firebase.js';
 
-class Goals extends Component{
-    render(){
-        return(
-            <p>Goals</p>
+class Goals extends Component {
+
+    constructor(props) {
+        super();
+        this.state = {
+            state: "state"
+        };
+    }
+
+    componentWillMount(){
+        console.log("WILL");
+
+    }
+
+    render() {
+        return (
+            <div>
+                {console.log("hi")}
+                <p>Goals</p>
+            </div>
         );
     }
 }
