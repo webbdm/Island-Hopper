a9ddd7f3950e8b7ee1c1807bfb73bb96aefb6c9d
webbdm <gwebb762@gmail.com>
2017-09-13 22:07:59 -0500

    Reconfigured Goals component with Promises

--- src/Goals.js
+++ src/Goals.js
@@ -3,25 +3,71 @@ import './App.css';
 //import { Link } from 'react-router-dom';
 import firebase from './firebase.js';
 
+
+const FoodItem = ({ name }) => (
+    <div className="white-text">
+        <p>{name}</p>
+    </div>
+);
+
 class Goals extends Component {
 
     constructor(props) {
         super();
         this.state = {
-            state: "state"
+            state: "state",
+            foods: [],
+            day: {},
+            user: props.data.user
         };
     }
 
-    componentWillMount(){
-        console.log("WILL");
+    componentWillMount() {
+        this.getFoods();
+        this.getAddedFoods();
+        console.log(this.state);
+    }
 
+    getFoods() {
+        const foodsRef = firebase.database().ref('foods');
+        foodsRef.once('value').then((snapshot) => {
+            let foods = snapshot.val();
+            let newState = [];
+            for (let food in foods) {
+                newState.push({
+                    id: food,
+                    protein: foods[food].protein,
+                    fat: foods[food].fat,
+                    carbs: foods[food].carbs,
+                    foodName: foods[food].foodName,
+                    cardCreator: foods[food].cardCreator
+                });
+            }
+
+            this.setState({
+                foods: newState
+            });
+        });
+    }
+
+    getAddedFoods() {
+        console.log("Get Added Foods");
     }
 
     render() {
         return (
-            <div>
-                {console.log("hi")}
+            <div className="white-text">
                 <p>Goals</p>
+
+                <div>
+                    <h3>{this.state.foods.map((food, index) => {
+                        return (
+                            <FoodItem
+                                key={index}
+                                name={food.foodName} />
+                        )
+                    })}</h3>
+                </div>
             </div>
         );
     }
